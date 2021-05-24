5a51b379b0a5b99ebc403948b3051e0da5e0b586
elena2020201 <elenarus1992@mail.ru>
2021-05-24 16:53:50 +0300

    Signed-off-by: Elena2020201 <elenarus1992@mail.ru>

---
+++ деление на 3 и 7, диал/CodeFile1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+class UsingIfDemo
+{
+    static void Main()
+    {
+        try
+        {
+            int number;
+            number = Int32.Parse(Interaction.InputBox("Введите число: ", "Ввод"));
+
+            if (number % 3 == 0 && number % 7 == 0) MessageBox.Show("Введенное число делится на 3 и 7", "Проверка деления числа на 3 и 7");
+            else MessageBox.Show("Число не подходит, введите другое");
+        }
+        catch
+        {
+            MessageBox.Show("Введите целое число!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show("Программа завершена", "Завершение");
+        }
+    }
+}
+   
